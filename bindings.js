1b4ae8815dcd8c9597ae09a09bfdc4521b5b4f35
Muhammad Safraz <mr.safraz@gmail.com>
2014-12-27 17:36:18 +0530

    use IconicJs global version & add iconic class

--- bindings.js
+++ bindings.js
@@ -1,11 +1,13 @@
-define(['knockout', 'durandal/binder', 'jquery', 'IconicJS'], function(ko, binder, $, IconicJS){
+define(['knockout', 'durandal/binder', 'jquery'], function(ko, binder, $){
 
-	var iconic = new IconicJS();
+	var iconic = IconicJS(); // use the global version of iconic library, because the AMD version has some issues
     var originalBinding = binder.binding;
 
     binder.binding = function(obj, view, instruction) {
     	$(view).find('[zf-iconic]').each(function(){
-    		iconic.inject(this);
+    		$(this).addClass('iconic');
+            var that = this;
+            iconic.inject(this);
     	});
     	originalBinding(obj, view, instruction);
     };
